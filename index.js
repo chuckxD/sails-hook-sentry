aa494ea14a80e2df3098fd79882b2dfefdcc1c0f
Alex Merrifield <alex.merrifield@levelupdevelopment.com>
2019-06-03 16:30:15 -0400

    update sentry npm package

--- index.js
+++ index.js
@@ -24,24 +24,24 @@ module.exports = function Sentry(sails) {
     initialize: function(cb) {
       var settings = sails.config[this.configKey];
       if (!settings.active) {
-        sails.log.verbose('Autoreload hook deactivated.');
+        sails.log.info('Autoreload hook deactivated.');
         return cb();
       }
 
       if (!settings.dsn) {
-        sails.log.verbose('DSN for Sentry is required.');
+        sails.log.info('DSN for Sentry is required.');
         return cb();
       }
 
-      var Raven = require('raven');
-      Raven.config(settings.dsn, settings.options).install();
+      const Sentry = require('@sentry/node');
+      Sentry.init({ dsn: settings.dsn, ...settings.options });
 
-      sails.sentry = Raven;
+      sails.sentry = Sentry;
 
       // handles Bluebird's promises unhandled rejections
       process.on('unhandledRejection', function(reason) {
         console.error('Unhandled rejection:', reason);
-        Raven.captureException(reason);
+        sentry.captureException(reason);
       });
 
       // We're done initializing.
